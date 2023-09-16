4d6db589774efdee3f5223a9529406fa3da51610
Evan Chisholm <elchisholm248@gmail.com>
2023-09-16 04:02:13 -0400

    added html element at the top of the page with the information

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 async function main() {
     console.log("----START OF FROM CREDIBLY----");
+    // chrome.browserAction.setPopup({
+    //     popup: "./index.html",
+    // });
     console.log(window.location.href);
 
     const res = await fetch(
@@ -7,6 +10,17 @@ async function main() {
     );
     const json = await res.json();
     console.log(json);
+    const factualness = document.createElement("p");
+    factualness.textContent = `Factualness: ${json.factualness}`;
+
+    const textbias = document.createElement("p");
+    textbias.textContent = `Bias: ${json.textBias}`;
+    const information = document.createElement("div");
+    information.appendChild(factualness);
+    information.appendChild(textbias);
+    // information.textContent = "Hello world";
+    document.body.appendChild(information);
+    document.body.insertBefore(information, document.body.firstChild);
 
     console.log("----END OF FROM CREDIBLY----");
 }
